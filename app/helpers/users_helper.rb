b4759c3315187517629c52244ab2dc67a7989373
Fábio Gomes <fabioxgn@gmail.com>
2017-08-10 20:50:41 -0300

    Adiciona form de cadastro

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def gravatar_for(email, name, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
+    image_tag(gravatar_url, alt: name, class: "gravatar")
+  end
 end
